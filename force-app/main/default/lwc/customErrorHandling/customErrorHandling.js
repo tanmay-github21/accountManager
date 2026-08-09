6cdcd945abd65b3f204a6083f8aa364f8e1bcfd9
tanmay-github21 <ilove2boogycode@Gmail.com>
2026-08-10 02:38:21 +0530

    Added basic javascript functionality

--- force-app/main/default/lwc/customErrorHandling/customErrorHandling.js
+++ force-app/main/default/lwc/customErrorHandling/customErrorHandling.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
-
-export default class CustomErrorHandling extends LightningElement {}
+import { LightningElement,track} from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+export default class CustomErrorHandling extends LightningElement {
+    isModalOpen=false; selectedErrorScenario='VALIDATION_ERROR'; @track formAccount={Name:''};
+    get errorScenarioOptions(){
+        return [{label:'Custom Field Validation Error', value:'VALIDATION_ERROR'}, 
+            {label:'System/DML Insert Exception', value:'DML_ERROR'},
+            {label:'No Error (Normal Insert)', value:'NONE'}
+        ];
+    }
+    openModal(){
+        this.formAccount = {Name:'Test Account'};
+        this.selectedErrorScenario='VALIDATION_ERROR';
+        this.isModalOpen=true;
+    }
+    closeModal(){this.isModalOpen=false;}
+}

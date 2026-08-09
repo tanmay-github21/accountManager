a50d08c293c1f18bc7d600b2a86c3e4efc5ca763
tanmay-github21 <ilove2boogycode@Gmail.com>
2026-08-10 02:43:30 +0530

    Modal Box Opening

--- force-app/main/default/lwc/customErrorHandling/customErrorHandling.js
+++ force-app/main/default/lwc/customErrorHandling/customErrorHandling.js
@@ -14,4 +14,15 @@ export default class CustomErrorHandling extends LightningElement {
         this.isModalOpen=true;
     }
     closeModal(){this.isModalOpen=false;}
+    handleErrorScenarioChange(event){
+        this.selectedErrorScenario=event.detail.value;
+        this.clearFieldErrors();
+    }
+    handleFormChange(event){
+        const field=event.target.dataset.field;
+        this.formAccount[field]=event.target.value;
+        event.target.setCustomValidity('');
+        event.target.reportValidity();
+    }
+
 }

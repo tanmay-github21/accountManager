b5e3eee0719106f56ae3291e33b20e4e8b805608
tanmay-github21 <ilove2boogycode@Gmail.com>
2026-08-22 21:55:59 +0530

    Added the event to the list component

--- force-app/main/default/lwc/dynamicInteractionsDetail/dynamicInteractionsDetail.js
+++ force-app/main/default/lwc/dynamicInteractionsDetail/dynamicInteractionsDetail.js
@@ -1,4 +1,14 @@
 import { LightningElement, track} from 'lwc';
+import getAccounts from '@salesforce/apex/dynamicAction_Accounts.getAccounts';
 export default class DynamicInteractionsDetail extends LightningElement {
-    
+    @track contacts=[];
+    connectedCallback(){
+        getAccounts().then(data=>this.contacts=data);
+    }
+    selectAccount(event){
+        event.preventDefault();
+        let accountId=event.currentTarget.dataset.accountId;
+        console.log('Account selected'+'\t'+accountId);
+        this.dispatchEvent(new CustomEvent('accountselected',{detail:{recordId:contactId}})); 
+    }
 }

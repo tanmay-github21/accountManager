34d90676c65f0a25bd3344c1bd74835cc5caa21f
tanmay-github21 <ilove2boogycode@Gmail.com>
2026-09-04 01:11:58 +0530

    TestMethod AddError keyword

--- force-app/main/default/lwc/dynamicInteractionsList/dynamicInteractionsList.js
+++ force-app/main/default/lwc/dynamicInteractionsList/dynamicInteractionsList.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
-
-export default class DynamicInteractionsList extends LightningElement {}
+import { LightningElement,track} from 'lwc';
+import getAccounts from '@salesforce/apex/dynamicAction_Accounts.getAccounts';
+export default class DynamicInteractionsList extends LightningElement {
+    @track accounts=[];
+    connectedCallback(){
+        getAccounts().then(data=>this.accounts=data);
+        console.log('kemcho majama'); 
+    }
+    handleSelect(event){
+        event.preventDefault();
+        let accountId=event.currentTarget.dataset.accountId;
+        console.log('Account id selected'+'\t'+accountId);
+        this.dispatchEvent(new CustomEvent('accountselected',{detail:{recordId:accountId}}));
+    }
+}
